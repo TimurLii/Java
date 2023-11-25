c5bf52ae7609b62fdf6706f72e21868f91b44533
Timur_2101 <timurka260197@gmil.com>
2023-11-25 11:38:08 +0200

    Лекция №2.Логирование.Работа с файлами.Обработка Ошибок.

---
+++ Lekcii/Lekcii2/program_lekcii_2.java
@@ -0,0 +1,19 @@
+package Lekcii.Lekcii2;
+
+public class program_lekcii_2 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i< 100;i++) {
+            sb.append("+");
+//            System.out.println(sb);
+        }
+        String str  = " Язык прогаммирования Java ";
+        String str_1 = "Язык программирвоания Python";
+        String sroka = str.concat(str_1);
+        System.out.println(sroka);
+
+    }
+
+
+}
+
