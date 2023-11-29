4d455a51769d2218186e5e3262d9151510c48151
Timur_2101 <timurka260197@gmil.com>
2023-11-29 20:07:11 +0200

    Youtube.Arrays

---
+++ youtube/array.java
@@ -0,0 +1,40 @@
+package youtube;
+
+import java.util.Arrays;
+
+public class array {
+    public static void main(String[] args) {
+//        int x = 4;
+//        int y = 5;
+////        int[] array = new int[2];
+////        array[0] = x;
+////        array[1] = y;
+//
+////        int[] array = new int[]{x,y};
+//
+//        int[] array = {x,y};
+//        System.out.println(Arrays.toString(array));
+//        System.out.println(array[0]);
+
+
+//        int [] arr = {1,2,3};
+//        int[] arr1 = new int[3];
+//        System.out.println(Arrays.toString(arr));
+//        System.out.println(Arrays.toString(arr1));
+//        arr[0] = 5;
+//        arr1[0] = 7;
+//        System.out.println(Arrays.toString(arr));
+//        System.out.println(Arrays.toString(arr1));
+
+
+//        Практическая задача.
+//        ответить на вопрос "Столица Катара" и записать в массив
+        char[] answer = {'Д','o','x','a'};
+//        answer[0] = "Д";
+//        answer[1] = "о";
+//        answer[2] = "х";
+//        answer[3] = "а";
+        System.out.println(answer);
+
+    }
+}
