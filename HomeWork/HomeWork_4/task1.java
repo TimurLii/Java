97c33e7929117ef98515b580fa11097fc85418d1
Timur_2101 <timurka260197@gmil.com>
2023-12-04 13:18:08 +0200

    Домашняя работа №4

---
+++ HomeWork/HomeWork_4/task1.java
@@ -0,0 +1,36 @@
+package HomeWork.HomeWork_4;
+
+import java.util.LinkedList;
+import java.util.Objects;
+
+public class task1 {
+    public static void main(String[] args) {
+        LinkedList<String> arr = new LinkedList<>();
+        arr.add("1");
+        arr.add("One");
+        arr.add("2");
+        arr.add("Two");
+//        System.out.println(arr);
+
+
+        System.out.println(revert(arr));
+
+    }
+
+    public static LinkedList<String> revert(LinkedList<String> arr) {
+//Пусть дан LinkedList с несколькими элементами.
+//Напишите класс LLTasks с методом revert, который принимал бы на вход LinkedList и возвращает “перевернутый” список.
+        LinkedList<String> newArr = new LinkedList<>();
+//        for (int i = 0; i <arr.size() ; i++) {
+//            String temp = arr.get(i);
+//            newArr.addFirst(temp);
+//
+//        }
+        for(String item:arr){
+            newArr.addFirst(item);
+        }
+
+
+        return newArr;
+    }
+}
---
+++ HomeWork/HomeWork_4/task3.java
@@ -0,0 +1,25 @@
+package HomeWork.HomeWork_4;
+
+import java.util.*;
+
+import static java.lang.Integer.valueOf;
+
+public class task3 {
+    public static void main(String[] args) {
+        calculater();
+    }
+
+    public static void calculater() {
+        LinkedList<Integer> list = new LinkedList<>();
+        Scanner scanner = new Scanner(System.in);
+        boolean work = true;
+        while (work) {
+            System.out.println("Введите команду: ");
+            String op = scanner.nextLine().toString();
+            Integer a = valueOf(scanner.nextLine());
+            Integer b = valueOf(scanner.nextLine());
+            int sum = 0;
+        }
+    }
+}
+
